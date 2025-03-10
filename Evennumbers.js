4acbd8a8874d8591bfad46e509c46c8e27c9ddd1
unknown <YY205@ccs.local>
2025-03-10 21:27:36 +0800

    removed js

--- Evennumbers.js
+++
@@ -1,9 +0,0 @@
-function filterEvenNumbers(numbers) {
-  return numbers.filter(function(number) {
-    return number % 2 === 0;
-  });
-}
-
-
-const result = filterEvenNumbers([1, 2, 3, 4, 5, 6]);
-console.log(result); // Output: [2, 4, 6]
